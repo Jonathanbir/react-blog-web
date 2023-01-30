59d14780b5b57758bfa437a8e29e90f2dfd90b4c
Jonathanbir <jonathan79111@gmail.com>
2023-01-30 15:14:48 +0800

    commit: 加入protected.js 用在 profile

--- routes/users/users.js
+++ routes/users/users.js
@@ -10,6 +10,7 @@ const {
   updateUserCtrl,
   logoutCtrl,
 } = require("../../controllers/users/users");
+const protected = require("../../middlewares/protected");
 
 const userRoutes = express.Router();
 
@@ -23,7 +24,7 @@ userRoutes.post("/login", loginCtrl);
 userRoutes.get("/:id", userDetailsCtrl);
 
 //GET/api/v1/users/profile/:id
-userRoutes.get("/profile/:id", profileCtrl);
+userRoutes.get("/profile/:id", protected, profileCtrl);
 
 //PUT/api/v1/users/profile-photo-upload/:id
 userRoutes.put("/profile-photo-upload/:id", uploadProfilePhotoCtrl);
